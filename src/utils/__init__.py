986587aa4ee52f2dc7fb7ac9784ef36a89e6ebf2
rayal-dargon <2312749625@qq.com>
2025-07-08 03:22:25 +0000

    add:the test of lora

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
-from .tools import print_rank_0, refresh_config, get_optimizer, set_up_trainable_param, init_swanlab_rank_0, swanlab_log_rank_0
+from .tools import print_rank_0, refresh_config, get_optimizer, set_up_trainable_param, init_swanlab_rank_0, swanlab_log_rank_0, pre_train_lora
 
 __all__ = [
     "print_rank_0",
@@ -7,4 +7,5 @@ __all__ = [
     "set_up_trainable_param",
     "init_swanlab_rank_0",
     "swanlab_log_rank_0",
+    "pre_train_lora",
 ]
