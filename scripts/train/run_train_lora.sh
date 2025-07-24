122ef247656c3de35890e2001b6a56e4fd9f1c7e
royal-dargon <2312749625@qq.com>
2025-07-24 12:54:13 +0000

    style:change the path to public path

--- scripts/train/run_train_lora.sh
+++ scripts/train/run_train_lora.sh
@@ -1,19 +1,19 @@
 enable_list="multimodal model.model.embed_tokens model.model.layers model.lm_head"
-experiment_name="Qwen3_0.6B_NT_sft_exp1_0724"
-output_path="/fs-computility/ai4agr/chenzihong/res_data_model/biomllm_res/${experiment_name}"
+experiment_name="Qwen3_4B_NT_sft_exp3_0724"
+output_path="/tos-bjml-ai4agr/lijinzhe/BioMLLM/RES_Model/${experiment_name}"
 
 options="--experiment-name $experiment_name \
 --output_dir $output_path \
 --use-lora \
---text-model-path /tos-bjml-ai4agr/lijinzhe/BioMLLM/Qwen3-0.6B \
+--text-model-path /tos-bjml-ai4agr/lijinzhe/BioMLLM/Qwen3-4B \
 --bio-model-path /tos-bjml-ai4agr/lijinzhe/BioModel/nucleotide-transformer/  \
 --multimodal-k-tokens 128 \
 --device cuda \
 --load-pretrained \
 --load_best_model_at_end True \
 --freeze-nt \
---train-dataset-path /tos-bjml-ai4agr/lijinzhe/dataset/BioMLLM/stage3_train_data/stage3_train_nt_dna_rna.parquet \
---eval-dataset-path /tos-bjml-ai4agr/lijinzhe/dataset/BioMLLM/stage3_train_data/val_nt_dna_rna.parquet \
+--train-dataset-path /tos-bjml-ai4agr/lijinzhe/dataset/BioMLLM/rewritten_8k/train_dna_rna.parquet \
+--eval-dataset-path /tos-bjml-ai4agr/lijinzhe/dataset/BioMLLM/rewritten_8k/valid_dna_rna.parquet \
 --max-len 1024 \
 --max-src-len 1024 \
 --eval-max-len 1024 \
@@ -22,8 +22,8 @@ options="--experiment-name $experiment_name \
 --mode sft \
 --per_device_train_batch_size 2 \
 --per_device_eval_batch_size 4 \
---read-nums 16500 \
---eval-read-nums 500 \
+--read-nums 3000 \
+--eval-read-nums 1000 \
 --num_train_epochs 3 \
 --learning_rate 1.0e-5 \
 --bf16 \
@@ -45,7 +45,7 @@ options="--experiment-name $experiment_name \
 " 
 # --save_safetensors \
 
-deepspeed --include localhost:0,1 \
+deepspeed --include localhost:0,1,2,3 \
 src/train_lora.py \
---deepspeed_config /tos-bjml-ai4agr/chenzihong/code/BioMLLM_V2/src/configs/zero2_config.json \
+--deepspeed_config /tos-bjml-ai4agr/lijinzhe/BioMLLM/dp_config/zero3_config.json \
 $options
