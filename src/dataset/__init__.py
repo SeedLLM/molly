191781ab71f98f60090ac7edc9a1d5ee0fd1dbf5
rayal-dargon <2312749625@qq.com>
2025-08-05 07:56:53 +0000

    feat:add the test dataset and collect

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,8 +1,8 @@
-from .omics_dataset import OmicsDataset, qwen_dna_collate_fn, DatasetConfig, OmicsTestDataset
+from .omics_dataset import OmicsDataset, qwen_dna_collate_fn, DatasetConfig, qwen_omics_collate_fn
 
 __all__ = [
     "OmicsDataset",
     "qwen_dna_collate_fn",
     "DatasetConfig",
-    "OmicsTestDataset",
+    "qwen_omics_collate_fn",
 ]
